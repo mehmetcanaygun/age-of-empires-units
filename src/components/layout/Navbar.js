e631a7f79d18f66b3c881ace5a9bf3f3589fa51f
mehmetcanaygun <mehmetcanaygun@gmail.com>
2021-11-27 15:25:51 +0300

    Create & Style Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,33 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return <header className="navbar">Navbar</header>;
+  return (
+    <header className="navbar">
+      <a href="/" className="logo">
+        LOGO
+      </a>
+      <nav className="navbar-nav">
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/units"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Units
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
