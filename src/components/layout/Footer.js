fec0bafa3db00363a0e43d9f48b6185d8742db4c
mehmetcanaygun <mehmetcanaygun@gmail.com>
2021-11-27 20:09:36 +0300

    Style Footer component

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,22 @@
 const Footer = () => {
-  return <footer className="footer">Footer</footer>;
+  return (
+    <footer className="footer">
+      <a href="/" className="logo">
+        <img src="./assets/logo-footer.png" alt="Logo" />
+        <span>Age of Empires Units</span>
+      </a>
+      <p className="footer-msg">
+        Made with <img src="/assets/heart.png" alt="Heart" /> by{" "}
+        <a
+          href="https://github.com/mehmetcanaygun"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Mehmet Can Aygün
+        </a>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
