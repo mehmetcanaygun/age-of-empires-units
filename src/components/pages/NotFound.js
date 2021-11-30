6d66dc479480b0249636fb8f40fb1f8e004645ca
mehmetcanaygun <mehmetcanaygun@gmail.com>
2021-11-30 18:22:08 +0300

    Add Not Found page

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
     <div className="page not-found-page">
-      <h2>Not Found Page</h2>
+      <div className="container">
+        <p className="title">
+          4<img src="/assets/axe.png" alt="Axe" />4
+        </p>
+        <p className="info">Page you're looking for could not be found...</p>
+        <Link to="/">Back to Home</Link>
+      </div>
     </div>
   );
 };
